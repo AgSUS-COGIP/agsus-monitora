f90418ba21db1f9da48fdde8aa616e3297910bb0
Yassury Sousa Suira <yassury.suira@agenciasus.org.br>
2026-07-02 09:39:18 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { hasSupabaseEnv } from "./lib/env.js";
+import "./styles/visual-polish.css";
 import "./modules/legacy-app.js";
+import { initVisualPolish } from "./modules/visual-polish.js";
+
+initVisualPolish();
 
 if (!hasSupabaseEnv()) {
   console.warn("Configure VITE_SUPABASE_URL e VITE_SUPABASE_PUBLISHABLE_KEY no Vercel.");
