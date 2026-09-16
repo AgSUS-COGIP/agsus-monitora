14676f109da0640874df04d16c21221352c5aefd
Yassury Sousa Suira <yassury.suira@agenciasus.org.br>
2026-09-16 10:06:27 -0300

    feat(arara): ativar efeito de fala no painel principal

--- src/main.js
+++ src/main.js
@@ -61,6 +61,7 @@ import { initHealthStatusDetailsRefinement } from "./modules/health-status-detai
 import { initHealthDetailsUx } from "./modules/health-details-ux.js";
 import { initHealthDetailsRuntimeFix } from "./modules/health-details-runtime-fix.js";
 import { initSidebarBranding } from "./modules/sidebar-branding.js";
+import { initAraraSpeakingEffects } from "./modules/arara-speaking-effects.js";
 import { aplicarLegendaDoMapaDetalhado } from "./modules/vinculos-territoriais.js";
 import { instalarAvisoDoPainelDeAcesso } from "./modules/aviso-de-contraste.js";
 import {
@@ -93,6 +94,7 @@ installFrontendPerformanceMonitor();
 installCspReportMonitor();
 initLoadingExperience();
 initVisualPolish();
+initAraraSpeakingEffects();
 initHealthIndigenousEnhancements();
 initHealthDashboardRefinementsSafe();
 initHealthDashboardInteractionFixes();
