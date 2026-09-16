f81d054fd4023bf3fa9608fa094d2555e3067ff8
Yassury Sousa Suira <yassury.suira@agenciasus.org.br>
2026-09-16 10:06:43 -0300

    feat(arara): ativar efeito de fala em Análises

--- src/analises/main.js
+++ src/analises/main.js
@@ -1,5 +1,6 @@
 import { installCsvBlobSecurityGuard } from "../lib/csv-security.js";
 import { updateAraraGuide } from "../modules/arara-guide.js";
+import { initAraraSpeakingEffects } from "../modules/arara-speaking-effects.js";
 import "../styles/arara-guide.css";
 import { installSessionLifecycle } from "../lib/session-lifecycle.js";
 import { installBackgroundResourceLifecycle } from "../lib/background-resource-lifecycle.js";
@@ -43,6 +44,7 @@ function compactAnalisesSessionTimer() {
 
 const embeddedInParentApp = window.parent !== window;
 if (!embeddedInParentApp) {
+  initAraraSpeakingEffects();
   updateAraraGuide(
     "analises",
     "Análises",
